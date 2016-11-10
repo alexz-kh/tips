1394952c3da675bc66503f7983e2681ee5e8f202
alexz <azvyagintsev@mirantis.com>
2016-11-10 13:01:52 +0200

    smart_gas: add sleep time
    * copy Animations func's
    * use LED_BRIGHTNESS from neopixel
    * bugfixs

--- co2/combo/smart_gas.py
+++ co2/combo/smart_gas.py
@@ -5,20 +5,23 @@ import requests
 import pytz, datetime, time
 import struct
 import array
-from neopixel import *
 from lib_sensors import HTU21D,mhz19
-from lib_led import my_ws_2812
 from Adafruit_BME280 import *
+from lib_led import my_ws_2812
 
 # Main program logic follows:
 if __name__ == '__main__':
 
         tz = pytz.timezone('Europe/Kiev')
-        lstrip = my_ws_2812()
+        # Sleeptime
+        timed_bright=1
+        now_h = (datetime.datetime.now()).hour
+        if now_h >= 8 and now_h <= 23:
+           timed_bright=15
+        lstrip = my_ws_2812(BRIGHTNESS=timed_bright)
         sensor_ht = BME280(mode=BME280_OSAMPLE_8, address=0x76)
         sensor_co2 = mhz19()
         c_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
-
 
         co2_value = sensor_co2.read_co2()
 
@@ -29,13 +32,13 @@ if __name__ == '__main__':
         pressure_value = hectopascals_value * 0.750064
         humid_value = sensor_ht.read_humidity()
 
-        lstrip.to_level(co2_value)
-
         print "{} \nCo2:{}. Temp:{}. Humidity:{}. Pressure:{}\n".format(c_time, co2_value, t_value, humid_value, pressure_value)
 
         # since co2_value could be None, we should skip whole string for thingspeak:
         co2_ts_string = "&field1={}".format(co2_value)
         if co2_value is None:
             co2_ts_string = None
+        # import ipdb;ipdb.set_trace()
+        lstrip.to_level(co2_value)
         thingspeak_key = os.environ.get("THINGSPEAK_KEY")
         requests.get('https://api.thingspeak.com/update?api_key={}{}&field2={}&field3={}&field4={}'.format(thingspeak_key,co2_ts_string,t_value,humid_value,pressure_value))

12d7232a90c2d5e468e2a57e3a33a4d0baa1fcb5
alexz <alexz.kh.ua@gmail.com>
2016-12-21 13:48:53 +0200

    migrate co2 to picore repo

--- co2/combo/cron.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-source /root/rpi/openrc
-/root/rpi/env_ws2812/bin/python /root/rpi/smart_gas/smart_gas.py >> /var/log/gas.log
--- co2/combo/cron_home.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-source /root/rpi/smart_gas/openrc
-/root/rpi/env_ws2812/bin/python /root/rpi/smart_gas/home_temp.py >> /var/log/temp.log

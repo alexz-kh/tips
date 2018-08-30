4dd168cd3d56006e607acc85cb5afe8dd73fd6bc
azvyagintsev <azvyagintsev@mirantis.com>
2018-08-30 14:22:17 +0300

    updates

--- oneshots/docker-offline-render/docker_spawn.sh
+++ oneshots/docker-offline-render/docker_spawn.sh
@@ -1,15 +1,17 @@
-#!/bin/bash
+#!/bin/bash -e
 
-code_tag="2018.4.0"
+#code_tag="2018.4.0"
+code_tag="master"
 #code_tag=testing
 
 srv_volumes=" -v /home/alexz/work/imgs/share/trash/dockers/volumes:/srv/volumes/aptly"
 nicev=" -v $(pwd)/_screenrc:/root/.screenrc:ro -v ${HOME}/.vimrc:/root/.vimrc:ro -v ${HOME}/.vim:/root/.vim:ro"
-volumes="${nicev} -v $(pwd):/test1 -v $(pwd)/mcp-offline-model:/srv/salt/reclass -v $(pwd)/salt-formulas-scripts:/srv/salt/scripts "
+volumes="${nicev} -v $(pwd):/test1 -v $(pwd)/model:/srv/salt/reclass -v $(pwd)/salt-formulas-scripts:/srv/salt/scripts "
 docker_image="ubuntu_16_quick_test:latest"
 opts=" ${volumes} -u root:root --ulimit nofile=4096:8192 --cpus=2"
 
-offline_model="https://github.com/Mirantis/mcp-offline-model"
+#offline_model="https://github.com/Mirantis/mcp-offline-model"
+offline_model="ssh://azvyagintsev@gerrit.mcp.mirantis.net:29418/salt-models/infra"
 
 # Flow
 # _prepare
@@ -17,9 +19,9 @@ offline_model="https://github.com/Mirantis/mcp-offline-model"
 # run_in_docker
 
 function _prepare(){
-  if [[ ! -d mcp-offline-model ]]; then
-    git clone --recursive ${offline_model} mcp-offline-model
-    pushd mcp-offline-model
+  if [[ ! -d model ]]; then
+    git clone --recursive ${offline_model} model
+    pushd model
       git checkout $code_tag
       git submodule update --init --recursive
     popd
@@ -53,16 +55,43 @@ function docker_run_apt01(){
   docker run --rm ${srv_volumes} ${opts} --hostname=apt01 -it ${docker_image} /bin/bash
 }
 
+function docker_run_ci.mcp.mirantis.net(){
+  #docker run --rm ${opts} -it docker-offline-render:2018.1 /bin/bash
+  docker run --rm ${srv_volumes} ${opts} --hostname=ci.mcp.mirantis.net -it ${docker_image} /bin/bash
+}
+
 function _local_refresh(){
   salt-call saltutil.clear_cache
   salt-call saltutil.refresh_pillar
   salt-call saltutil.sync_all
 }
+### _prepare_ parts
+
+
+function _prepare_ci.mcp.mirantis.net(){
+  set -x
+  cp -Lfrv packer-templates/mirror-image/files/* /
+  rm -vf salt-formulas-scripts/.salt-master-setup.sh.passed
+  rm -v /done_ubuntu_salt_bootstrap
+  rm -rvf '/srv/salt/reclass/nodes/_generated/'
+  #
+  export CLUSTER_NAME="mcp_ci_prd"
+  export FORMULA_VERSION=testing # from where install salt-formulas
+  export PACKER_OFFLINE_BUILD=true
+  # salt_bootstrap.sh related
+  export BS_HOSTNAME=$(hostname -f)
+  export CLUSTER_DOMAIN='mcp.mirantis.net'
+  bash -x packer-templates/mirror-image/scripts/salt_bootstrap.sh || true
+  _local_refresh
+#
+
+  echo "DONE _prepare_ci.mcp.mirantis.net"
+}
 
 function _prepare_apt01(){
 
   set -x
-  cp -rav packer-templates/mirror-image/files/* /
+  cp -Lfrv packer-templates/mirror-image/files/* /
   rm -vf salt-formulas-scripts/.salt-master-setup.sh.passed
   #
   export CLUSTER_NAME="mcp-offline"
@@ -88,14 +117,22 @@ function run_in_docker(){
   echo "deb [arch=amd64] http://apt.mirantis.com/xenial testing extra" > /etc/apt/sources.list.d/temp-mcp_salt.list
   echo "deb [arch=amd64] http://apt.mirantis.com/xenial testing salt"  >> /etc/apt/sources.list.d/temp-mcp_salt.list
   apt-get update
-  apt-get intall reclass gnupg jq -y
+  apt-get install -y reclass gnupg jq
   rm -v /etc/apt/sources.list.d/temp-mcp_salt.list
 
-  if [[ $(hostname -f ) == "apt01" ]]; then
-  _prepare_apt01
-  else
-    echo "wrong hostname?"
-  fi
+  _hostname=$(hostname -f)
+
+  case $_hostname in
+       apt01)
+            _prepare_apt01
+            ;;
+       ci.mcp.mirantis.net)
+            _prepare_ci.mcp.mirantis.net
+            ;;
+       *)
+            echo "wrong hostname?"
+            ;;
+  esac
 #
 }
 # apt01.mcp-offline.local
